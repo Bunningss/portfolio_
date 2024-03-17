3d5f231c05589044b9f5e7218044cfa788817cdf
bunningss <mahmudovi183@gmail.com>
2024-03-17 20:32:58 +0600

    style update

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -2,11 +2,16 @@ import { FC } from "react";
 import styles from "./Navigation.module.css";
 import Container from "../Container/Container";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/images/M.png";
 
 const Navigation: FC = () => {
   return (
     <div className={styles.navigation}>
       <Container>
+        <Link href="/">
+          <Image src={logo} alt="logo" className={styles.logo} />
+        </Link>
         <div className={styles.col}>
           <Link href="/" className={styles.links}>
             home
